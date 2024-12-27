f1e62e5693d1ba53f444531a4d86fd9eccbf1e0e
lilimanzo <lmanzo1@uci.edu>
2024-12-27 10:35:09 -0800

    test244 mmsv=1

--- driver-mct/main/mrg_mod.F90
+++ driver-mct/main/mrg_mod.F90
@@ -225,7 +225,7 @@ contains
           fraci = fractions_a%Rattr(kif,n)
           fraco = fractions_a%Rattr(kof,n)
 
-          mmsv  = x2a_a%rAttr(index_x2a_Faxx_lwup,n) !/ (5.67e-8_r8 * x2a_a%rAttr(index_x2a_Sx_t,n) ** 4) ! LM added
+          mmsv  = 1 !x2a_a%rAttr(index_x2a_Faxx_lwup,n) / (5.67e-8_r8 * x2a_a%rAttr(index_x2a_Sx_t,n) ** 4) ! LM added
           x2a_a%rAttr(index_x2a_Sx_mmsv,n) = mmsv ! LM added
 
           if (lindx(ka) > 0 .and. fracl > 0._r8) then ! LM test
