6837f3d8aa7d8bda6e65c7fd3f118e9afcecddba
lilimanzo <lmanzo1@uci.edu>
2024-12-29 15:57:26 -0800

    test248 continue adding mmsv to prep_atm

--- driver-mct/main/prep_atm_mod.F90
+++ driver-mct/main/prep_atm_mod.F90
@@ -504,6 +504,7 @@ contains
     index_x2a_Sf_lfrac = mct_aVect_indexRA(x2a_a,'Sf_lfrac')
     index_x2a_Sf_ifrac = mct_aVect_indexRA(x2a_a,'Sf_ifrac')
     index_x2a_Sf_ofrac = mct_aVect_indexRA(x2a_a,'Sf_ofrac')
+    index_x2a_Sx_mmsv  = mct_aVect_indexRA(x2a_a,'Sx_mmsv')  ! LM added
 
     do n = 1,lsize
        x2a_a%rAttr(index_x2a_Sf_lfrac,n) = fractions_a%Rattr(klf,n)
