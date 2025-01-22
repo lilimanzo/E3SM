e96e79f7ca445bd9c89050373459ad693ea0b951
lilimanzo <lmanzo1@uci.edu>
2025-01-22 14:35:16 -0800

    test294 remove radtemp=0 seq_flux_mct

--- driver-mct/main/seq_flux_mct.F90
+++ driver-mct/main/seq_flux_mct.F90
@@ -333,9 +333,9 @@ contains
     allocate(lwup(nloc),stat=ier)
     if(ier/=0) call mct_die(subName,'allocate lwup',ier)
     lwup = 0.0_r8
-    allocate(radtemp(nloc),stat=ier) ! LM added
-    if(ier/=0) call mct_die(subName,'allocate radtemp',ier) ! LM added
-    radtemp = 0.0_r8 ! LM added
+    allocate(radtemp(nloc),stat=ier)                            ! LM added
+    if(ier/=0) call mct_die(subName,'allocate radtemp',ier)     ! LM added
+    !radtemp = 0.0_r8                                           ! LM added
     allocate(taux(nloc),stat=ier)
     if(ier/=0) call mct_die(subName,'allocate taux',ier)
     taux = 0.0_r8
