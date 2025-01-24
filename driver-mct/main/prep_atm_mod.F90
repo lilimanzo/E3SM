a019b63fce2749c90a3cc1765bd7faab105436c4
lilimanzo <lmanzo1@uci.edu>
2025-01-24 15:34:21 -0800

    test321 keep making runnable

--- driver-mct/main/prep_atm_mod.F90
+++ driver-mct/main/prep_atm_mod.F90
@@ -45,7 +45,7 @@ module prep_atm_mod
   public :: prep_atm_get_mapper_Si2a
   public :: prep_atm_get_mapper_Fi2a
 
-  !public :: prep_atm_msv ! LM added
+  public :: prep_atm_msv ! LM added
 
   !--------------------------------------------------------------------------
   ! Private interfaces
