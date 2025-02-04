8a83c35f9b33ad04ecc229f5f68cfe7e3d1011fa
lilimanzo <lmanzo1@uci.edu>
2025-02-03 16:58:50 -0800

    test338 add Si_taf to coupler

--- driver-mct/shr/seq_flds_mod.F90
+++ driver-mct/shr/seq_flds_mod.F90
@@ -1726,6 +1726,14 @@ contains
     attname  = 'Sl_lmsv'
     call metadata_set(attname, longname, stdname, units)
 
+    ! LM variable for temporal adjustment factor
+    call seq_flds_add(i2x_states,"Si_taf")
+    longname = 'LM temporal adjustment factor'
+    stdname  = 'LM_taf'
+    units    = '1'
+    attname  = 'Si_taf'
+    call metadata_set(attname, longname, stdname, units)
+    
     ! LM variable for mean emissivity computed in coupler
     call seq_flds_add(x2a_states,"Sx_mmsv")
     longname = 'Mean emissivity'
