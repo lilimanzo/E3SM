f924ba86db37eef7431db733cb65592ef1e05bcf
lilimanzo <lmanzo1@uci.edu>
2025-02-26 10:10:34 -0800

    test507 test shr_const_ocn_msv

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2748,7 +2748,7 @@ contains
 
                 ! spatial adjustment factor
                 x2a_a % rAttr(index_x2a_Sx_saf, n) = x2a_a % rAttr(index_x2a_Faxx_lwup, n) &
-                        * frac_a % rAttr(ki, n)
+                        * frac_a % rAttr(ki, n) * shr_const_ocn_msv
                 !x2a_a % rAttr(index_x2a_Sx_saf, n) = ki
         enddo
 
