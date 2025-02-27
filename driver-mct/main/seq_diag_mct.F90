e59c00b1c8b56235c0c598e9f76733f715b63048
lilimanzo <lmanzo1@uci.edu>
2025-02-27 14:18:46 -0800

    test521 saf**.25

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2751,9 +2751,9 @@ contains
                 ! spatial adjustment factor
                 !x2a_a % rAttr(index_x2a_Sx_saf, n) = x2a_a % rAttr(index_x2a_Faxx_lwup, n) &
                 !        * frac_a % rAttr(ki, n)
-                x2a_a % rAttr(index_x2a_Sx_saf, n) = ( x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n) &
+                x2a_a % rAttr(index_x2a_Sx_saf, n) = ( x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) &
                         / (shr_const_stebol * ( frac_a%rAttr(ki,n) * (frac_a%rAttr(ki,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) &
-                        )  )**4  - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) ) 
+                        )**0.25  )**4  - a2x_a%rAttr(index_a2x_Faxa_lwdn,n))
                 !a2x_a % rAttr(index_a2x_Faxa_lwdn, n) !frac_a % rAttr(kir, n)
         enddo
 
