73ee2175f48630497928f59f1b8e7c0e04a15205
lilimanzo <lmanzo1@uci.edu>
2025-02-28 16:28:57 -0800

    test544 get back to 539

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2752,12 +2752,13 @@ contains
                 x2a_a%rAttr(index_x2a_Sl_taf,n) = 1.0
 
                 ! spatial adjustment factor
-                x2a_a % rAttr(index_x2a_Sx_saf, n) = frac_a%rAttr(ko,n) 
+                !x2a_a % rAttr(index_x2a_Sx_saf, n) = frac_a%rAttr(ko,n) 
                 
-                !x2a_a % rAttr(index_x2a_Sx_saf, n) = ( x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) &
-                        !/ (shr_const_stebol * ( frac_a%rAttr(ki,n) * ( (frac_a%rAttr(ki,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) &
-                        !-(1-x2a_a%rAttr(index_x2a_Si_taf,n))*a2x_a%rAttr(index_a2x_Faxa_lwdn,n)  ) &
-                        !/ (x2a_a%rAttr(index_x2a_Si_taf,n) * shr_const_stebol))**0.25 & 
+                x2a_a % rAttr(index_x2a_Sx_saf, n) = ( x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) &
+                        / (shr_const_stebol * ( frac_a%rAttr(ki,n) * ( (frac_a%rAttr(ki,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) &
+                        -(1-x2a_a%rAttr(index_x2a_Si_taf,n))*a2x_a%rAttr(index_a2x_Faxa_lwdn,n)  ) &
+                        / (x2a_a%rAttr(index_x2a_Si_taf,n) * shr_const_stebol))**0.25 & 
+                        )**4 - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) 
                         !+ frac_a%rAttr(ko,n) &
                         !* ( (frac_a%rAttr(ko,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) &
                         !-(1-shr_const_ocn_msv)*a2x_a%rAttr(index_a2x_Faxa_lwdn,n)  ) &
