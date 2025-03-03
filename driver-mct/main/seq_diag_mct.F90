472062a0b9c57743f343247910161b1ea093a5ac
lilimanzo <lmanzo1@uci.edu>
2025-03-02 19:32:31 -0800

    test559 +1 to check if NaNs are operable

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2760,7 +2760,7 @@ contains
                         / (shr_const_stebol * (frac_a%rAttr(ki,n) * ((-frac_a%rAttr(ki,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) - &
                         (1 - x2a_a%rAttr(index_x2a_Si_taf,n)) * a2x_a%rAttr(index_a2x_Faxa_lwdn,n) ) & 
                         / ( shr_const_stebol * x2a_a%rAttr(index_x2a_Si_taf,n) ))**.25 + frac_a%rAttr(ko,n) &
-                        )**4 - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) 
+                        )**4 - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) + 1
                         !/ (shr_const_stebol * ( frac_a%rAttr(ki,n) * ( (frac_a%rAttr(ki,n) * x2a_a%rAttr(index_x2a_Faxx_lwup,n) &
                         !-(1-x2a_a%rAttr(index_x2a_Si_taf,n))*a2x_a%rAttr(index_a2x_Faxa_lwdn,n)  ) &
                         !/ (x2a_a%rAttr(index_x2a_Si_taf,n) ))**0.25 & 
