209a86202ca20dff932f3d2e9495b079c3a1b2aa
lilimanzo <lmanzo1@uci.edu>
2025-03-11 14:17:53 -0700

    test635a get TsfKr back, add radiativeTemperature to registry

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2756,8 +2756,6 @@ contains
                 x2a_a%rAttr(index_x2a_Sl_taf,n) = 1.0
 
                 ! spatial adjustment factor
-                !x2a_a % rAttr(index_x2a_Sx_saf, n) = frac_a%rAttr(ko,n) 
-                
                 ! (Fup - Fdn) / (stebol * (ifrac* ((iFup - (1-e1)Fdn)/(stebol*e1))**.25 + ofrac )**4 - fdn)
                 x2a_a % rAttr(index_x2a_Sx_saf, n) = &
                         (-x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) & 
