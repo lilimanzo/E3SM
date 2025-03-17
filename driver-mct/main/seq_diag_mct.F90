eb3aaff1007bd6f113c18d60eb540ada4f19322d
lilimanzo <lmanzo1@uci.edu>
2025-03-17 10:09:16 -0700

    test677 fractional (1-eo) term in saf

--- driver-mct/main/seq_diag_mct.F90
+++ driver-mct/main/seq_diag_mct.F90
@@ -2758,14 +2758,14 @@ contains
                 x2a_a%rAttr(index_x2a_Sl_taf,n) = 1.0
 
                 ! spatial adjustment factor
-                ! (Fup-Fdn) / (sb * (((iFup-(1-ei)*iFdn)/(ei*sb))**.25 + ((oFup-0)/(eo*sb))**.25 + ((lFup-(0))/sb)**.25 -Fdn)**4 )
+                ! (Fup-Fdn) / (sb * (((iFup-(1-ei)*iFdn)/(ei*sb))**.25 + ((oFup-(1-oeo))/(eo*sb))**.25 + ((lFup-(0))/sb)**.25 -Fdn)**4 )
                 x2a_a % rAttr(index_x2a_Sx_saf, n) = &
                         (-x2a_a%rAttr(index_x2a_Faxx_lwup,n) - a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) &
                         / (shr_const_stebol * ((((-frac_a%rAttr(kir,n)*x2a_a%rAttr(index_x2a_Faxx_lwup,n)) &
                         -(1-x2a_a%rAttr(index_x2a_Sl_taf,n))*frac_a%rAttr(kir,n)*a2x_a%rAttr(index_a2x_Faxa_lwdn,n)) &
                         /(x2a_a%rAttr(index_x2a_Sl_taf,n)*shr_const_stebol))**.25 & 
                         + ((-frac_a%rAttr(kor,n)*x2a_a%rAttr(index_x2a_Faxx_lwup,n)  &
-                        - (0) ) / (shr_const_ocn_msv*shr_const_stebol))**.25 & 
+                        - (1-frac_a%rAttr(kor,n)*shr_const_ocn_msv) ) / (shr_const_ocn_msv*shr_const_stebol))**.25 & 
                         + ((-frac_a%rAttr(kl,n)*x2a_a%rAttr(index_x2a_Faxx_lwup,n)-(0))/shr_const_stebol)**.25) &
                         **4 - a2x_a%rAttr(index_a2x_Faxa_lwdn,n))
                 
