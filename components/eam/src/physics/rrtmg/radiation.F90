31449db330980b594fad00371d1c60085befbea1
lilimanzo <lmanzo1@uci.edu>
2025-03-24 17:23:14 -0700

    test727 add begchunk,endchunk to loop

--- components/eam/src/physics/rrtmg/radiation.F90
+++ components/eam/src/physics/rrtmg/radiation.F90
@@ -1109,9 +1109,12 @@ end function radiation_nextsw_cday
     call pbuf_get_field(pbuf, ldp_idx, ldp) ! LM added
 
     if (is_first_restart_step()) then ! LM added if statement
-       do i = 1,ncol ! LM added loop
-          do k = 1,pver
-             m = pver-k+1
+       do    c=begchunk,endchunk
+          do i = 1,ncol ! LM added loop
+             do k = 1,pver
+                m = pver-k+1
+                lwdn_prev(i,:,k) = rstate(c)%lwdn(i,m) 
+             end do
           end do
        end do
     end if
