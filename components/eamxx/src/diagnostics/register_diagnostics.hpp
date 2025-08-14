mahf708/scream

set up skeleton for aerocom diags

components/eamxx/src/diagnostics/register_diagnostics.hpp
@@ -22,6 +22,7 @@
 #include "diagnostics/wind_speed.hpp"
 #include "diagnostics/aodvis.hpp"
 #include "diagnostics/number_path.hpp"
+#include "diagnostics/aerocom_cldtop.hpp"
 
 namespace scream {
 
@@ -51,6 +52,7 @@ inline void register_diagnostics () {
   diag_factory.register_product("wind_speed",&create_atmosphere_diagnostic<WindSpeed>);
   diag_factory.register_product("AerosolOpticalDepth550nm",&create_atmosphere_diagnostic<AODVis>);
   diag_factory.register_product("NumberPath",&create_atmosphere_diagnostic<NumberPathDiagnostic>);
+  diag_factory.register_product("AeroComCldTop",&create_atmosphere_diagnostic<AeroComCldTop>);
 }
 
 } // namespace scream

lilimanzo/E3SM

components/eamxx/src/diagnostics/register_diagnostics.hpp
@@ -22,6 +22,7 @@
 #include "diagnostics/wind_speed.hpp"
 #include "diagnostics/aodvis.hpp"
 #include "diagnostics/number_path.hpp"
+#include "diagnostics/aerocom_cld.hpp"
 
 namespace scream {
 
@@ -51,6 +52,7 @@ inline void register_diagnostics () {
   diag_factory.register_product("wind_speed",&create_atmosphere_diagnostic<WindSpeed>);
   diag_factory.register_product("AerosolOpticalDepth550nm",&create_atmosphere_diagnostic<AODVis>);
   diag_factory.register_product("NumberPath",&create_atmosphere_diagnostic<NumberPathDiagnostic>);
+  diag_factory.register_product("AeroComCld",&create_atmosphere_diagnostic<AeroComCld>);
 }
 
 } // namespace scream